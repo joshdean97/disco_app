45cfd5bd878ef49ac1b42925edcda828efd6e9e0
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2026-06-01 09:31:15 +0100

    Fix views indentation

--- authentication/views.py
+++ authentication/views.py
@@ -12,7 +12,7 @@ from .forms import (
     StaffSkillsForm,
 )
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from ..disco_app.emails import send_welcome_email
+from .emails import send_welcome_email
 
 
 def register(request):
@@ -43,10 +43,6 @@ def register(request):
                 )
 
                 send_welcome_email(user)
-
-messages.success(
-    request, "Account created successfully. You can now log in."
-)
 
                 messages.success(
                     request, "Account created successfully. You can now log in."
